6af6bde68f7d798064ef78fff2d81dee112a5ceb
Peter <43998116+PetercSnk@users.noreply.github.com>
2022-07-27 11:45:54 +0100

    removed dsp

--- DSP Practice/W4/Image_Proc/ditherbw_eg.m
+++
@@ -1,8 +0,0 @@
-% Load Image and Show
-im = imread('lenabw.BMP'); 
-imshow(im)
-
-% Dither Image and Show Result
-figure('Name', 'Dithered Image');
-imdith = dither(im);
-imshow(imdith);
--- DSP Practice/W4/Image_Proc/videoreaderdemo.m
+++
@@ -1,9 +0,0 @@
-vr = VideoReader('origdave.avi');
-vidFrames = read(vr);
-numFrames = get(vr, 'NumberOfFrames');
-for k = 1:numFrames
-    mov(k).cdata = vidFrames(:, :, :, k);
-    mov(k).colormap = [];
-end
-
-movie(mov, 1, get(vr, 'FrameRate'));
